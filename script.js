34b2a7a8f35524b48050cee07983612fe442d353
nicksam00 <nickmanishere007@gmail.com>
2025-08-10 17:16:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let w = (c.width = window.innerWidth),
 (hh = h / 2),
   (opts = {
     // change the text in here //
-    strings: ["HAPPY", "BIRTHDAY!", "to You" "Babeee"],
+    strings: ["HAPPY", "BIRTHDAY!", "to You", "Babeee"],
     charSize: 30,
     charSpacing: 35,
     lineHeight: 40,
@@ -399,5 +399,6 @@ window.addEventListener("resize", function () {
 
   ctx.font = opts.charSize + "px Verdana";
 });
+
 
 
